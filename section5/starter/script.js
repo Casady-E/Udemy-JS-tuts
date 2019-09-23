59fb93d4639e45232e0bc992800e2f6b3b044481
Ethan Casady <ethancasady1@gmail.com>
2019-09-23 00:52:46 -0600

    Return functions within functions practice. Create different interview questions and return specific functions based on input.

--- section5/starter/script.js
+++ section5/starter/script.js
@@ -136,3 +136,32 @@ console.log(ages);
 console.log(fullAges);
 console.log(rates);
 */
+
+
+/*
+//functions returning functions
+
+function interviewQuestion(job){
+  if(job === 'designer'){
+    return function(name){
+      console.log(name + ', can you please explain what UX design is?');
+    }
+  } else if (job === 'teacher'){
+    return function(name){
+      console.log('What subject do you teach, ' + name + '?');
+    }
+  } else {
+    return function(name){
+      console.log('Hello ' + name + ', what do you do?');
+    }
+  }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer')
+
+teacherQuestion('John');
+designerQuestion('John')
+
+interviewQuestion('teacher')('Mark');
+*/
