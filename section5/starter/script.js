e0c63a3b05af626b6f7c6f65bc41944a243476e7
Ethan Casady <ethancasady1@gmail.com>
2019-09-24 00:28:18 -0600

    Practice closure with functions, allows for child functions to return variables from already completed functions. Attempt to recreate older function with closure method but after failure use final version from lecture.

--- section5/starter/script.js
+++ section5/starter/script.js
@@ -190,3 +190,76 @@ game();
 */
 
 //closures
+
+function retirement(retirementAge){
+return function(yearOfBirth){
+  var a = ' years left until retirement.'
+  var age = 2019 - yearOfBirth;
+  console.log((retirementAge - age) + a);
+}
+}
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement (67);
+retirementUS(1990);
+retirementGermany(1990);
+retirementIceland(1990);
+
+//retirement(66)(1990)
+
+/*
+function interviewQuestion(job){
+  if(job === 'designer'){
+    return function(name){
+      console.log(name + ', can you please explain what UX design is?');
+    }
+  } else if (job === 'teacher'){
+    return function(name){
+      console.log('What subject do you teach, ' + name + '?');
+    }
+  } else {
+    return function(name){
+      console.log('Hello ' + name + ', what do you do?');
+    }
+  }
+}
+*/
+/*
+//my failed attempt
+function interviewJobQuestions(job, name){
+  var designer = 'designer';
+  var teacher = 'teacher';
+  var other = (job != 'designer') || (job != 'teacher');
+    return function(designer){
+    console.log(name + ', can you please explain what UX design is?');
+    }
+    return function(teacher){
+    console.log('What subject do you teach, ' + name + '?');
+    }
+    return function(other){
+    console.log('Hello ' + name + ', what do you do?');
+    }
+}
+var john = interviewJobQuestions('designer', 'john');
+var jane = interviewJobQuestions('teacher', 'jane');
+var harold = interviewJobQuestions('butt', 'harold');
+john();
+jane();
+harold();
+*/
+function interviewQuestion(job){
+  return function(name){
+    if (job === 'designer'){
+      console.log(name + ', can you please explain what UX design is?');
+    } else if (job === 'teacher'){
+      console.log('What subject do you teach, ' + name + '?');
+    } else {
+      console.log('Hello ' + name + ', what do you do?');
+    }
+  }
+}
+
+interviewQuestion('teacher')('John');
+interviewQuestion('designer')('Jane');
+interviewQuestion('baller')('Kobe');
