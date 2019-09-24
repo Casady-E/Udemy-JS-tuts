7b33525f16e04b0695c9c01ed79a9fa23fe64363
Ethan Casady <ethancasady1@gmail.com>
2019-09-23 23:34:48 -0600

    Create iffy practice functions. iffy can be called once and is not associated with an assigned variable, creates new scope hidden outside main scope, create data privacy and doesnt interfere with variables.

--- section5/starter/script.js
+++ section5/starter/script.js
@@ -165,3 +165,25 @@ designerQuestion('John')
 
 interviewQuestion('teacher')('Mark');
 */
+
+//immediately invoked function expressions, IIFE
+
+/*
+function game(){
+  var score = math.random() * 10;
+  console.log(score >= 5);
+}
+game();
+*/
+
+(function (){
+  var score = Math.random() * 10;
+  console.log(score >= 5);
+})();
+
+//console.log(score);
+
+(function (goodLuck){
+  var score = Math.random() * 10;
+  console.log(score >= 5 - goodLuck);
+})(5);
